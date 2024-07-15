05455f038aafbfdd484f496c9c1a81b7b97e8101
Tushar Singh <149937568+Tushar-Deshwal-tech@users.noreply.github.com>
2024-07-15 18:28:58 +0000

    create responsive login website

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Landing.css';
+
+function Landing() {
+  return (
+    <div>
+      <div className='main'>
+        <div className='content'></div>
+      </div>
+    </div>
+  );
+}
+
+export default Landing;
